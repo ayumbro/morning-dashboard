58f78b4bc33a9b53cf359e96aca71494a83ac6b7
ayumbro <ayumbro@gmail.com>
2022-09-04 20:11:23 +0800

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient();
+
+function MorningBoard({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MorningBoard;
---
+++ pi-setup.md
@@ -0,0 +1,8 @@
+Download Raspberry Pi Imager
+https://www.raspberrypi.com/software/
+
+Launch Pi Imager
+Select Raspberry Pi OS Lite 32bit
+Select the SD card
+Setup Hostname, SSH Login and Wifi credentials
+Burn the image
